af254f79fcf537cd103f111d4c219a5526473800
JJenk4th <jjenkins4th@gmail.com>
2014-08-27 18:37:21 -0700

    Search/Delete

--- search.php
+++ search.php
@@ -10,15 +10,21 @@ if(isset($_SESSION["name"])){
 	$results=$dbh->query("Select `name`,`id` from `users`");
 echo "<table border=\"1\">";
 while ($row=$results->fetch()) {
-	echo "<tr><td>{$row->id}</td><td>{$row->name}</td></tr>";
+	echo "<tr><td>{$row->id}</td><td>{$row->name}</td><td><a href=\"search.php?id={$row->id}&submit=delete\">Be Gone!</a></td></tr>";
 }
 
 echo "</table>";
 
+if (isset($_POST["submit"]) AND $_POST["submit"]=='search')
 
+	echo "Select * from `users` where like \"{$_POST["name"]}\"";
 
+if (isset($_GET["submit"]) AND $_GET["submit"]=='delete')
 
-}else{
+	echo "Delete from `users` where = \"{$_GET["name"]}\" ";
+}
+
+else{
 
 	echo "<div>You're Not Logged In</div>";
 }
@@ -33,4 +39,4 @@ echo "</table>";
 	<p>Search Users</p>
 	<form  method="post" action="search.php"  id="searchform">
 	<input  type="text" name="name"> 
-	<input  type="submit" name="submit" value="Search">
+	<input  type="submit" name="submit" value="search">
